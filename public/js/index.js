9432cd6b9de10479fc678214991f6dd0a3840e93
Daniel Fernandez <dani@bebot.design>
2021-10-03 15:25:56 -0300

    M08C02 terminado

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 import addClaseBody from "./Components/claseBody.js";
 import { addClassImpares, addClassPares } from "./Components/darClase.js";
 import darColor from "./Components/darColor.js";
+import secretoMagico from "./Components/MaquinaEstado.js";
+import toggleMenu from "./Components/Menu.js";
 import pedirNombre from "./Components/pedirNombre.js";
 import upperCaseStyle from "./Components/upperCaseStyle.js";
 
@@ -10,6 +12,9 @@ import upperCaseStyle from "./Components/upperCaseStyle.js";
     const $h2 = d.querySelector('h2')
     const $a = d.querySelector('a')
     const $p = d.querySelectorAll('p')
+    
+    // Maquina estado secreto
+    secretoMagico('secreto', 'SECRETO MAGICO');
     
     $main.display = "block"
     // Pido el nombre
@@ -28,5 +33,16 @@ import upperCaseStyle from "./Components/upperCaseStyle.js";
     addClassPares($p, "destacadoPar")
     addClassImpares($p, "destacadoImpar")
 
+    // Mostrar Menu
+    toggleMenu(".logoDH", "menu")
+
+    // Eventos MouseOver
+    const colorh2 = $h2.style.color;
+    d.addEventListener('mouseover', e => {
+      if(e.target === $h2) darColor($h2, 'tomato')
+    })
+    d.addEventListener('mouseout', e => {
+      if(e.target === $h2) darColor($h2, colorh2)
+    })
   })
 })(document);
